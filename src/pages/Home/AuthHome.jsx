1ac8ab85a0ebf13be921d8c2c43c47b61fb2e627
Naikharini <harininaik04@gmail.com>
2026-01-03 20:34:32 +0530

    Fix casing in AuthHome.css import and remove postinstall script from package.json

--- src/pages/Home/AuthHome.jsx
+++ src/pages/Home/AuthHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaUserPlus, FaSignInAlt, FaCheckCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import "./authHome.css";
+import "./AuthHome.css";
 
 const AuthHome = () => {
   const navigate = useNavigate();
